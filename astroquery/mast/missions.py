6bb66f5ac81a61e846ffe984c00b6b518e86a91e
Sam Bianco <sbianco@stsci.edu>
2024-10-29 16:42:37 -0400

    Get products and download from MastMissions

--- astroquery/mast/missions.py
+++ astroquery/mast/missions.py
@@ -6,16 +6,19 @@ MAST Missions
 This module contains methods for searching MAST missions.
 """
 
+from urllib.parse import quote
+import numpy as np
 import requests
 import warnings
+from pathlib import Path
 
-from astropy.table import Table
+from astropy.table import Table, Row
 import astropy.units as u
 import astropy.coordinates as coord
 
 from astroquery.utils import commons, async_to_sync
 from astroquery.utils.class_or_instance import class_or_instance
-from astroquery.exceptions import InvalidQueryError, MaxResultsWarning
+from astroquery.exceptions import InvalidQueryError, MaxResultsWarning, NoResultsWarning
 
 from astroquery.mast import utils
 from astroquery.mast.core import MastQueryWithLogin
@@ -32,17 +35,23 @@ class MastMissionsClass(MastQueryWithLogin):
     Class that allows direct programmatic access to retrieve metadata via the MAST search API for a given mission.
     """
 
-    def __init__(self, *, mission='hst', service='search'):
+    def __init__(self, *, mission='hst'):
         super().__init__()
 
         self._search_option_fields = ['limit', 'offset', 'sort_by', 'search_key', 'sort_desc', 'select_cols',
                                       'skip_count', 'user_fields']
-        self.service = service
         self.mission = mission
+        self.mission_kwds = {
+            'hst': {'dataset_id': 'sci_data_set_name'},
+            'jwst': {'dataset_id': 'fileSetName'}
+        }
         self.limit = 5000
 
-        service_dict = {self.service: {'path': self.service, 'args': {}}}
-        self._service_api_connection.set_service_params(service_dict, f"{self.service}/{self.mission}")
+        self.service = 'search'
+        service_dict = {'search': {'path': 'search', 'args': {}},
+                        'list_products': {'path': 'list_products', 'args': {}},
+                        'retrieve_product': {'path': 'retrieve_product', 'args': {}}}
+        self._service_api_connection.set_service_params(service_dict, f'search/{self.mission}')
 
     def _parse_result(self, response, *, verbose=False):  # Used by the async_to_sync decorator functionality
         """
@@ -62,7 +71,11 @@ class MastMissionsClass(MastQueryWithLogin):
         response : `~astropy.table.Table`
         """
 
-        results = self._service_api_connection._parse_result(response, verbose, data_key='results')
+        if self.service == 'search':
+            results = self._service_api_connection._parse_result(response, verbose, data_key='results')
+        elif self.service == 'list_products':
+            results = Table(response.json()['products'])
+
         if len(results) >= self.limit:
             warnings.warn("Maximum results returned, may not include all sources within radius.",
                           MaxResultsWarning)
@@ -103,6 +116,7 @@ class MastMissionsClass(MastQueryWithLogin):
         """
 
         self.limit = limit
+        self.service = 'search'
 
         # Put coordinates and radius into consistent format
         coordinates = commons.parse_coordinates(coordinates)
@@ -169,6 +183,7 @@ class MastMissionsClass(MastQueryWithLogin):
         """
 
         self.limit = limit
+        self.service = 'search'
 
         if objectname or coordinates:
             coordinates = utils.parse_input_location(coordinates, objectname)
@@ -229,6 +244,272 @@ class MastMissionsClass(MastQueryWithLogin):
         coordinates = utils.resolve_object(objectname)
 
         return self.query_region_async(coordinates, radius=radius, limit=limit, offset=offset, **kwargs)
+
+    @class_or_instance
+    def get_prodict_list_async(self, datasets):
+        """
+        Test
+        """
+        self.service = 'list_products'
+
+        # Getting array of dataset IDs
+        if isinstance(datasets, Table) or isinstance(datasets, Row):
+            datasets = datasets[self.mission_kwds[self.mission]['dataset_id']]
+        if isinstance(datasets, str):
+            datasets = np.array([datasets])
+        if isinstance(datasets, list):
+            datasets = np.array(datasets)
+
+        datasets = datasets[datasets != '']
+        if datasets.size == 0:
+            raise InvalidQueryError("Dataset list is empty, no associated products.")
+
+        params = {'dataset_ids': ','.join(datasets)}
+
+        return self._service_api_connection.service_request_async(self.service, params, method='GET', use_json=True)
+
+    def filter_products(self, products, *, extension=None, **filters):
+        """
+        Takes an `~astropy.table.Table` of mission data products and filters it based on given filters.
+
+        Parameters
+        ----------
+        products : `~astropy.table.Table`
+            Table containing data products to be filtered.
+        extension : string or array, optional
+            Default None. Option to filter by file extension.
+        **filters :
+            Filters to be applied.
+            The column name is the keyword, with the argument being one or more acceptable values
+            for that parameter.
+            Filter behavior is AND between the filters and OR within a filter set.
+            For example: type="science", extension=["fits","jpg"]
+
+        Returns
+        -------
+        response : `~astropy.table.Table`
+        """
+
+        filter_mask = np.full(len(products), True, dtype=bool)
+
+        # Applying extension filter
+        if extension:
+            if isinstance(extension, str):
+                extension = [extension]
+
+            mask = np.full(len(products), False, dtype=bool)
+            for elt in extension:
+                mask |= [False if isinstance(x, np.ma.core.MaskedConstant) else x.endswith(elt)
+                         for x in products["filename"]]
+            filter_mask &= mask
+
+        # Applying the rest of the filters
+        for colname, vals in filters.items():
+
+            if isinstance(vals, str):
+                vals = [vals]
+
+            mask = np.full(len(products), False, dtype=bool)
+            for elt in vals:
+                mask |= (products[colname] == elt)
+
+            filter_mask &= mask
+
+        return products[np.where(filter_mask)]
+
+    def download_file(self, uri, *, local_path=None, cache=True, verbose=True):
+        """
+        Downloads a single file based on the data URI
+
+        Parameters
+        ----------
+        uri : str
+            The product dataURI, e.g. mast:JWST/product/jw00736-o039_t001_miri_ch1-long_x1d.fits
+        local_path : str
+            Directory or filename to which the file will be downloaded.  Defaults to current working directory.
+        cache : bool
+            Default is True. If file is found on disk it will not be downloaded again.
+        verbose : bool, optional
+            Default True. Whether to show download progress in the console.
+
+        Returns
+        -------
+        status: str
+            download status message.  Either COMPLETE, SKIPPED, or ERROR.
+        msg : str
+            An error status message, if any.
+        url : str
+            The full url download path
+        """
+
+        # create the full data URL
+        base_url = self._service_api_connection.MISSIONS_DOWNLOAD_URL + self.mission + '/api/v0.1/retrieve_product'
+        data_url = base_url + "?product_name=" + uri
+        escaped_url = base_url + "?product_name=" + quote(uri, safe=":")
+
+        # parse a local file path from local_path parameter.  Use current directory as default.
+        filename = Path(uri).name
+        if not local_path:  # local file path is not defined
+            local_path = filename
+        else:
+            path = Path(local_path)
+            if not path.suffix:  # local_path is a directory
+                local_path = path / filename  # append filename
+                if not path.exists():  # create directory if it doesn't exist
+                    path.mkdir(parents=True, exist_ok=True)
+
+        status = "COMPLETE"
+        msg = None
+        url = None
+
+        try:
+            self._download_file(escaped_url, local_path,
+                                cache=cache, continuation=False,
+                                verbose=verbose)
+
+            # check if file exists also this is where would perform md5,
+            # and also check the filesize if the database reliably reported file sizes
+            if (not Path(local_path).is_file()) and (status != "SKIPPED"):
+                status = "ERROR"
+                msg = "File was not downloaded"
+                url = data_url
+
+        except requests.HTTPError as err:
+            status = "ERROR"
+            msg = "HTTPError: {0}".format(err)
+            url = data_url
+
+        return status, msg, url
+
+    def _download_files(self, products, base_dir, *, flat=False, cache=True, verbose=True):
+        """
+        Takes an `~astropy.table.Table` of data products and downloads them into the directory given by base_dir.
+
+        Parameters
+        ----------
+        products : `~astropy.table.Table`
+            Table containing products to be downloaded.
+        base_dir : str
+            Directory in which files will be downloaded.
+        flat : bool
+            Default is False.  If set to True, no subdirectories will be made for the
+            downloaded files.
+        cache : bool
+            Default is True. If file is found on disk it will not be downloaded again.
+        verbose : bool, optional
+            Default True. Whether to show download progress in the console.
+
+        Returns
+        -------
+        response : `~astropy.table.Table`
+        """
+
+        manifest_array = []
+        for data_product in products:
+
+            # create the local file download path
+            if not flat:
+                local_path = Path(base_dir, data_product['dataset'])
+                local_path.mkdir(parents=True, exist_ok=True)
+            else:
+                local_path = base_dir
+            local_path = Path(local_path) / Path(data_product['filename']).name
+
+            # download the files
+            status, msg, url = self.download_file(data_product['uri'], local_path=local_path,
+                                                  cache=cache, verbose=verbose)
+
+            manifest_array.append([local_path, status, msg, url])
+
+        manifest = Table(rows=manifest_array, names=('Local Path', 'Status', 'Message', "URL"))
+
+        return manifest
+
+    def download_products(self, products, *, download_dir=None, flat=False,
+                          cache=True, extension=None, verbose=True, **filters):
+        """
+        Download data products.
+        If cloud access is enabled, files will be downloaded from the cloud if possible.
+
+        Parameters
+        ----------
+        products : str, list, `~astropy.table.Table`
+            Either a single or list of dataset IDs (as can be given to `get_product_list`),
+            or a Table of products (as is returned by `get_product_list`)
+        download_dir : str, optional
+            Optional.  Directory to download files to.  Defaults to current directory.
+        flat : bool, optional
+            Default is False.  If set to True, and download_dir is specified, it will put
+            all files into download_dir without subdirectories.  Or if set to True and
+            download_dir is not specified, it will put files in the current directory,
+            again with no subdirs.  The default of False puts files into the standard
+            directory structure of "mastDownload/<obs_collection>/<obs_id>/".  If
+            curl_flag=True, the flat flag has no effect, as astroquery does not control
+            how MAST generates the curl download script.
+        cache : bool, optional
+            Default is True. If file is found on disc it will not be downloaded again.
+            Note: has no affect when downloading curl script.
+        curl_flag : bool, optional
+            Default is False.  If true instead of downloading files directly, a curl script
+            will be downloaded that can be used to download the data files at a later time.
+        extension : string or array, optional
+            Default None. Option to filter by file extension.
+        verbose : bool, optional
+            Default True. Whether to show download progress in the console.
+        **filters :
+            Filters to be applied.  Valid filters are all products fields returned by
+            ``get_metadata("products")`` and 'extension' which is the desired file extension.
+            The Column Name (or 'extension') is the keyword, with the argument being one or
+            more acceptable values for that parameter.
+            Filter behavior is AND between the filters and OR within a filter set.
+            For example: productType="SCIENCE",extension=["fits","jpg"]
+
+        Returns
+        -------
+        response : `~astropy.table.Table`
+            The manifest of files downloaded, or status of files on disk if curl option chosen.
+        """
+        # If the products list is a row we need to cast it as a table
+        if isinstance(products, Row):
+            products = Table(products, masked=True)
+
+        # If the products list is not already a table of products we need to
+        # get the products and filter them appropriately
+        if not isinstance(products, Table):
+
+            if isinstance(products, str):
+                products = [products]
+
+            # collect list of products
+            product_lists = []
+            for oid in products:
+                product_lists.append(self.get_product_list(oid))
+
+            products = np.vstack(product_lists)
+
+        # apply filters
+        products = self.filter_products(products, extension=extension, **filters)
+
+        # remove duplicate products
+        products = utils.remove_duplicate_products(products, 'uri')
+
+        if not len(products):
+            warnings.warn("No products to download.", NoResultsWarning)
+            return
+
+        # set up the download directory and paths
+        download_dir = '.' if not download_dir else download_dir
+
+        if flat:
+            base_dir = download_dir
+        else:
+            base_dir = Path(download_dir, "mastDownload")
+        manifest = self._download_files(products,
+                                        base_dir=base_dir, flat=flat,
+                                        cache=cache,
+                                        verbose=verbose)
+
+        return manifest
 
     @class_or_instance
     def get_column_list(self):
